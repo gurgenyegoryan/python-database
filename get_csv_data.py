62641131ccb68c298c2f9cb96702b97be11607ea
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-11-21 13:19:24 +0400

    Resolve proble with execute rows values format

--- get_csv_data.py
+++ get_csv_data.py
@@ -1,40 +1,20 @@
 import csv
+
 # import pandas as pd
-#
-# # read CSV file
-# results = pd.read_csv('Individual_Incident_2004.csv')
-#
-# # count no. of lines
-# print(type(results))
-# print("Number of lines present:-",
-#       len(results))
-# Open file
 csv_file = "Individual_Incident_2004.csv"
 with open(csv_file) as file_obj:
-    # Skips the heading
-    # Using next() method
-
-
+    # Get columns name
     csv_data = csv.reader(open(csv_file))
     columns_name = next(csv_data)
     # Create reader object by passing the file
     # object to reader method
-    reader_obj = csv.reader(file_obj)
-
-    reader_obj_transfer = reader_obj
     # Iterate over each row in the csv file
     # using reader object
     list_elements_type = []
-
-    for row in reader_obj:
-        for element in row:
-            try:
-                type(int(element))
-                list_elements_type.append('int NOT NULL')
-            except:
-                type(element)
-                list_elements_type.append('longtext')
-        break
-
-print(len(columns_name))
-print(len(list_elements_type))
+    for row in next(csv_data):
+        try:
+            int(row)
+        except ValueError:
+            list_elements_type.append('longtext')
+        else:
+            list_elements_type.append('int')
--- main.py
+++ main.py
@@ -1,69 +1,44 @@
 import mariadb
 import connect_mariadb_server
 import get_csv_data
+import create_check_table
 
+# Get variables
 cur = connect_mariadb_server.conn.cursor()
-create_table = connect_mariadb_server.create_table
+table_name = connect_mariadb_server.table_name
 column_names = get_csv_data.columns_name
 values_type = get_csv_data.list_elements_type
-database_name = 'data1_big'
-# Create database if not exist
-j = 0
-while j < 2:
-    if create_table is None:
-        s = f"create table {database_name} ({column_names[0]} {values_type[0]});"
-        cur.execute(s)
-        create_table = f'{database_name}'
-    else:
-        for i in range(1, len(column_names)):
-            try:
-                s = f"alter table {database_name} add ({column_names[i]} {values_type[i]});"
-                cur.execute(s)
-            except mariadb.Error as e:
-                print(f"You have column with this name: {e}")
-    j += 1
-# Add data in file
+csv_data = get_csv_data.csv_data
 
-reader_obj = get_csv_data.csv_data
-column_names_final = ''
-
-# for i in column_names:
-#     column_names_final = column_names_final + i + ','
-
-values_count = 49 * '?,'
+#Get values count that be set
+values_count = len(column_names) * '?,'
 values_count = values_count.rstrip(values_count[-1])
-cur.execute(f"SHOW COLUMNS FROM {database_name}")
-for i in cur:
-    column_names_final = column_names_final + i[0] + ','
 
+# Translate column names to the desired form in maria db
+column_names_final = ''
+for i in column_names:
+    column_names_final = column_names_final + i + ','
 column_names_final = column_names_final.rstrip(column_names_final[-1])
-k = 0
-for row in reader_obj:
-    # print(row[0])
-    # print(type(row[0]))
-    # for i in row:
-    #     print(i)
-    #     print(type(i))
-    #     break
-    #     if type(i) == str:
-    #         i = '\"' + i + '\"'
-    #     roww = roww + i + ','
-    # break
-    # roww = roww.rstrip(roww[-1])
-    # print(type(roww))
-    # break
-    # sql = f"insert into data1_big ({heading}) values ({roww});"
 
+# Set rows in table
+row_final = []
+for row in csv_data:
+    for i in row:
+        if i == '':
+            i = 0
+        try:
+            int(i)
+        except ValueError:
+            row_final.append(i)
+        else:
+            row_final.append(int(i))
+    row_final = tuple(row_final)
     try:
-        cur.execute(f"INSERT INTO {database_name} ({column_names_final}) VALUES ({values_count})", (row))
-        # cur.execute(f"INSERT INTO data1_big ({column_names_final}) VALUES ({roww});")
-        print('row set: ')
+        cur.execute(f"INSERT INTO {table_name} ({column_names_final}) VALUES ({values_count})", row_final)
     except mariadb.Error as e:
         print(f"Error: {e}")
-
-    #cur.commit()
+    else:
+        print('row set: ')
+    break
+    cur.commit()
 cur.close()
-    #cur.execute(f"INSERT INTO data1_big ({column_names_final}) VALUES ({roww});")
-    # conn.commit()
-    #
-    # print("Row inserted")
