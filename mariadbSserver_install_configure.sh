2bfc701508d64e6ce0b7cf3fa977cba295ece36f
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-11-22 10:31:28 +0400

    Modified

--- mariadbSserver_install_configure.sh
+++ mariadbSserver_install_configure.sh
@@ -4,11 +4,11 @@ yum install mariadb-server -y
 systemctl start mariadb
 
 
-root_temp_pass=$(grep 'A temporary password' /var/log/mysqld.log |tail -1 |awk '{split($0,a,": "); print a[2]}')
-echo "root_temp_pass:"$root_temp_pass
+#root_temp_pass=Yeg.1995
+#echo "root_temp_pass:"$root_temp_pass
 
 cat > mysql_secure_installation.sql << EOF
-UPDATE mysql.user SET Password=PASSWORD('root_pass') WHERE User='root';
+UPDATE mysql.user SET Password=PASSWORD('Yeg.1995') WHERE User='root';
 # Kill the anonymous users
 DELETE FROM mysql.user WHERE User='';
 # disallow remote login for root
