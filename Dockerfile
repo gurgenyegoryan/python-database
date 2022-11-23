05c9b6e833ee52b8e8a7ee2a7b02fdef684a3008
gurgen.yegoryan <gurgen.yegoryan@gmail.com>
2022-11-23 12:26:08 +0400

    Modiifed mariadb script and Dockerfile,added working script into Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,7 @@ RUN wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup \
     && echo "367a80b01083c34899958cdd62525104a3de6069161d309039e84048d89ee98b  mariadb_repo_setup" \
     | sha256sum -c -
 
-# Install mariadb server
-RUN apt install mariadb-server -y
+
 
 RUN chmod +x mariadb_repo_setup \
     && ./mariadb_repo_setup \
@@ -35,6 +34,9 @@ RUN apt-get install libc6 \
 
 WORKDIR /app
 COPY mariadbSserver_install_configure.sh mariadbSserver_install_configure.sh
+
+RUN chmod +x mariadbSserver_install_configure.sh \
+    && ./mariadbSserver_install_configure.sh
 
 
 #CMD [python3, main.py]
--- mariadbSserver_install_configure.sh
+++ mariadbSserver_install_configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Install mariadb server
+apt install mariadb-server -y
 service mariadb start
 
 DEBIAN_FRONTEND=noninteractive apt-get -yq install expect
@@ -34,22 +36,5 @@ echo "$SECURE_MYSQL"
 
 
 mysql -e "CREATE DATABASE pythontest /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysql -e "CREATE USER root@localhost IDENTIFIED BY 'Yeg.1995';"
 mysql -e "GRANT ALL PRIVILEGES ON pythontest.* TO 'root'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
-#apt update
-#apt install mariadb-server -y
-#service mariadb start
-#root_temp_pass=Yeg.1995
-#echo "root_temp_pass:"$root_temp_pass
-
-#cat > mysql_secure_installation.sql << EOF
-#UPDATE mysql.user SET Password=PASSWORD('Yeg.1995') WHERE User='root';
-## Kill the anonymous users
-#DELETE FROM mysql.user WHERE User='';
-## disallow remote login for root
-##DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-## Make our changes take effect
-#FLUSH PRIVILEGES;
-#EOF
-#mysql -uroot -p"$Yeg.1995" --connect-expired-password <mysql_secure_installation.sql
